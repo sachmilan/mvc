2ec856cd17d6ba31571c3c6a95bb61a94319da32
Sachmilan <sachmilansingh@gmail.com>
2022-07-26 10:12:26 -0600

    asd

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./user');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const Post = require('./post');
+const Comment = require('./comment');
 
 // User - Post association
 User.hasMany(Post, {
